9199ad5a4ae7d349f26275c24ad4abe31f599a23
RyuMinSu <sonicsu94@gmail.com>
2022-12-31 15:52:10 +0900

    221231_justcnn

---
+++ JustCNN.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import PIL
+
+import tensorflow as tf
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+    
+print(tf.__version__)
